25486cdb92f2b3c2404ef50cb4c25e709ec3df8b
Andrew Razumovsky <grimones00@gmail.com>
2018-01-16 06:30:56 +0200

    Initial commit

---
+++ server/src/styled-jsx-server-main.ts
@@ -0,0 +1,264 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import {
+	createConnection, IConnection, TextDocuments, InitializeParams, InitializeResult, ServerCapabilities
+} from 'vscode-languageserver';
+
+import { TextDocument } from 'vscode-languageserver-types';
+
+import { ConfigurationRequest } from 'vscode-languageserver-protocol/lib/protocol.configuration.proposed';
+import { DocumentColorRequest, ServerCapabilities as CPServerCapabilities, ColorPresentationRequest } from 'vscode-languageserver-protocol/lib/protocol.colorProvider.proposed';
+
+import { getCSSLanguageService, LanguageSettings, Stylesheet } from 'vscode-css-languageservice';
+import { getLanguageModelCache } from './language-model-cache';
+
+import { getStyledJsx, getStyledJsxUnderCursor } from './styled-jsx-utils'
+
+
+// Create a connection for the server.
+let connection: IConnection = createConnection();
+
+console.log = connection.console.log.bind(connection.console);
+console.error = connection.console.error.bind(connection.console);
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents: TextDocuments = new TextDocuments();
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+let stylesheets = getLanguageModelCache<Stylesheet>(10, 60, document => cssLanguageService.parseStylesheet(document));
+documents.onDidClose(e => {
+	stylesheets.onDocumentRemoved(e.document);
+});
+connection.onShutdown(() => {
+	stylesheets.dispose();
+});
+
+let scopedSettingsSupport = false;
+// After the server has started the client sends an initilize request. The server receives
+// in the passed params the rootPath of the workspace plus the client capabilities.
+connection.onInitialize((params: InitializeParams): InitializeResult => {
+	function hasClientCapability(name: string) {
+		let keys = name.split('.');
+		let c: any = params.capabilities;
+		for (let i = 0; c && i < keys.length; i++) {
+			c = c[keys[i]];
+		}
+		return !!c;
+	}
+	let snippetSupport = hasClientCapability('textDocument.completion.completionItem.snippetSupport');
+	scopedSettingsSupport = hasClientCapability('workspace.configuration');
+	let capabilities: ServerCapabilities & CPServerCapabilities = {
+		// Tell the client that the server works in FULL text document sync mode
+		textDocumentSync: documents.syncKind,
+		completionProvider: snippetSupport ? { resolveProvider: false } : undefined,
+		hoverProvider: true,
+		documentSymbolProvider: true,
+		referencesProvider: true,
+		definitionProvider: true,
+		documentHighlightProvider: true,
+		codeActionProvider: true,
+		renameProvider: true,
+		colorProvider: true
+	};
+	return { capabilities };
+});
+
+const cssLanguageService = getCSSLanguageService();
+
+let documentSettings: { [key: string]: Thenable<LanguageSettings | undefined> } = {};
+// remove document settings on close
+documents.onDidClose(e => {
+	delete documentSettings[e.document.uri];
+});
+function getDocumentSettings(textDocument: TextDocument): Thenable<LanguageSettings | undefined> {
+	if (scopedSettingsSupport) {
+		let promise = documentSettings[textDocument.uri];
+		if (!promise) {
+			let configRequestParam = { items: [{ scopeUri: textDocument.uri, section: 'css' }] };
+			promise = connection.sendRequest(ConfigurationRequest.type, configRequestParam).then(s => s[0]);
+			documentSettings[textDocument.uri] = promise;
+		}
+		return promise;
+	}
+	return Promise.resolve(void 0);
+}
+
+// The settings have changed. Is send on server activation as well.
+connection.onDidChangeConfiguration(change => {
+	updateConfiguration(<LanguageSettings>change.settings.css);
+});
+
+function updateConfiguration(settings: LanguageSettings) {
+	cssLanguageService.configure(settings)
+	// reset all document settings
+	documentSettings = {};
+	// Revalidate any open text documents
+	documents.all().forEach(triggerValidation);
+}
+
+let pendingValidationRequests: { [uri: string]: NodeJS.Timer } = {};
+const validationDelayMs = 200;
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+	triggerValidation(change.document);
+});
+
+// a document has closed: clear all diagnostics
+documents.onDidClose(event => {
+	clearDiagnostics(event.document);
+});
+
+function clearDiagnostics(document: TextDocument) {
+	cleanPendingValidation(document);
+	connection.sendDiagnostics({ uri: document.uri, diagnostics: [] });
+}
+
+function cleanPendingValidation(textDocument: TextDocument): void {
+	let request = pendingValidationRequests[textDocument.uri];
+	if (request) {
+		clearTimeout(request);
+		delete pendingValidationRequests[textDocument.uri];
+	}
+}
+
+function triggerValidation(textDocument: TextDocument): void {
+	cleanPendingValidation(textDocument);
+	pendingValidationRequests[textDocument.uri] = setTimeout(() => {
+		delete pendingValidationRequests[textDocument.uri];
+		validateTextDocument(textDocument);
+	}, validationDelayMs);
+}
+
+function validateTextDocument(document: TextDocument): void {
+	let settingsPromise = getDocumentSettings(document);
+	settingsPromise.then(settings => {
+		const styledJsx = getStyledJsx(document, stylesheets);
+		if (styledJsx) {
+			const { cssDocument, stylesheet } = styledJsx;
+			let diagnostics = cssLanguageService.doValidation(cssDocument, stylesheet, settings);
+			connection.sendDiagnostics({ uri: document.uri, diagnostics });
+		}
+		else {
+			clearDiagnostics(document);
+		}
+	});
+}
+
+connection.onCompletion(textDocumentPosition => {
+	const document = documents.get(textDocumentPosition.textDocument.uri);
+	const cursorOffset = document.offsetAt(textDocumentPosition.position);
+
+	const styledJsx = getStyledJsxUnderCursor(document, stylesheets, cursorOffset);
+	
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.doComplete(cssDocument, textDocumentPosition.position, stylesheet)
+	}
+	return null;
+});
+
+connection.onHover(textDocumentPosition => {
+	let document = documents.get(textDocumentPosition.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.doHover(cssDocument, textDocumentPosition.position, stylesheet)
+	}
+	return null;
+});
+
+connection.onDocumentSymbol(documentSymbolParams => {
+	let document = documents.get(documentSymbolParams.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.findDocumentSymbols(cssDocument, stylesheet);
+	}
+	return null;
+});
+
+connection.onDefinition(documentSymbolParams => {
+	let document = documents.get(documentSymbolParams.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.findDefinition(cssDocument, documentSymbolParams.position, stylesheet);
+	}
+	return null;
+});
+
+connection.onDocumentHighlight(documentSymbolParams => {
+	let document = documents.get(documentSymbolParams.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.findDocumentHighlights(cssDocument, documentSymbolParams.position, stylesheet);
+	}
+	return null;
+});
+
+connection.onReferences(referenceParams => {
+	let document = documents.get(referenceParams.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.findReferences(cssDocument, referenceParams.position, stylesheet);
+	}
+	return null;
+});
+
+connection.onCodeAction(codeActionParams => {
+	let document = documents.get(codeActionParams.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.doCodeActions(cssDocument, codeActionParams.range, codeActionParams.context, stylesheet);
+	}
+	return null;
+});
+
+connection.onRequest(DocumentColorRequest.type, params => {
+	let document = documents.get(params.textDocument.uri);
+	if (document) {
+		const styledJsx = getStyledJsx(document, stylesheets);
+		if (styledJsx) {
+			const { cssDocument, stylesheet } = styledJsx;
+			return cssLanguageService.findDocumentColors(cssDocument, stylesheet);
+		}
+	}
+	return [];
+});
+
+connection.onRequest(ColorPresentationRequest.type, params => {
+	let document = documents.get(params.textDocument.uri);
+	if (document) {
+		const styledJsx = getStyledJsx(document, stylesheets);
+		if (styledJsx) {
+			const { cssDocument, stylesheet } = styledJsx;
+			return cssLanguageService.getColorPresentations(cssDocument, stylesheet, params.color, params.range);
+		}
+	}
+	return [];
+});
+
+connection.onRenameRequest(renameParameters => {
+	let document = documents.get(renameParameters.textDocument.uri);
+	const styledJsx = getStyledJsx(document, stylesheets);
+	if (styledJsx) {
+		const { cssDocument, stylesheet } = styledJsx;
+		return cssLanguageService.doRename(cssDocument, renameParameters.position, renameParameters.newName, stylesheet);
+	}
+	return null;
+});
+
+// Listen on the connection
+connection.listen();
---
+++ server/src/styled-jsx-utils.ts
@@ -0,0 +1,157 @@
+import * as ts from 'typescript';
+import { TextDocument } from 'vscode-languageserver-types';
+import { Stylesheet } from 'vscode-css-languageservice';
+import { LanguageModelCache } from './language-model-cache';
+
+
+export interface StyledJsxTaggedTemplate {
+	start: number;
+	end: number
+}
+
+export interface StyledJsxTagAttributes {
+	firstAttributeName: string | undefined;
+	secondAttributeName: string | undefined
+}
+
+export interface StyledJsx {
+	cssDocument: TextDocument;
+	stylesheet: Stylesheet;
+}
+
+const styledJsxPattern = /((<\s*?style\s*?(global)?\s*?jsx\s*?(global)?\s*?>)|(\s*?css\s*?`))/g;
+export function getApproximateStyledJsxOffsets(document: TextDocument): number[] {
+	const results = [];
+	const doc = document.getText();
+	while (styledJsxPattern.exec(doc)) {
+		results.push(styledJsxPattern.lastIndex);
+	}
+	return results;
+}
+
+// css`button { position: relative; }`
+export function isStyledJsxTaggedTemplate(token: any): boolean {
+	if (token.kind === ts.SyntaxKind.TaggedTemplateExpression) {
+		if (token.tag.getText() === 'css') {
+			return true;
+		}
+	}
+	return false;
+}
+
+function getStyleTagAttributeNames(openingElement: ts.JsxOpeningElement): StyledJsxTagAttributes {
+	const { properties } = openingElement.attributes;
+	const firstAttribute = properties[0];
+	const secondAttribute = properties[1];
+	const firstAttributeName = firstAttribute && firstAttribute.name!.getText();
+	const secondAttributeName = secondAttribute && secondAttribute.name!.getText();
+
+	return {
+		firstAttributeName,
+		secondAttributeName
+	}
+}
+
+// Is <style jsx/>. Maybe there is a better name for function
+export function isStyledJsxTag(token: ts.Node) {
+	if (token.kind === ts.SyntaxKind.JsxElement) {
+		const openingElement: ts.JsxOpeningElement = (token as ts.JsxElement).openingElement;
+		const closingElement: ts.JsxClosingElement = (token as ts.JsxElement).closingElement;
+		// Check that opening and closing tags are 'style'
+		if (openingElement.tagName.getText() === 'style' && closingElement.tagName.getText() === 'style') {
+			const { firstAttributeName, secondAttributeName } = getStyleTagAttributeNames(openingElement);
+			// Check that opening element has 'jsx' with optional 'global' attribute.
+			if (firstAttributeName === 'jsx' || (firstAttributeName === 'global' && secondAttributeName === 'jsx')) {
+				const nextToken = (ts as any).findNextToken(openingElement, openingElement.parent);
+				// Check if there is a '{'
+				if (nextToken.kind === ts.SyntaxKind.FirstPunctuation) {
+					const anotherNextToken = (ts as any).findNextToken(nextToken, nextToken.parent);
+					// Check if there is a beginning of the template string. This is neccessary to skip things like <style jsx>{styles}</style>
+					if (anotherNextToken.kind === ts.SyntaxKind.FirstTemplateToken || anotherNextToken.kind === ts.SyntaxKind.TemplateHead) {
+						return true;
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+
+export function findStyledJsxTaggedTemplate(textDocument: TextDocument, cursorOffsets: number[]) : StyledJsxTaggedTemplate[] {
+	const source = ts.createSourceFile('tmp', textDocument.getText(), ts.ScriptTarget.Latest, true, ts.ScriptKind.JSX | ts.ScriptKind.TSX);
+
+	const result: StyledJsxTaggedTemplate[] = [];
+	for (let i = 0; i < cursorOffsets.length; i++) {
+		let token = (ts as any).getTokenAtPosition(source, cursorOffsets[i])
+		while (token) {
+			if (isStyledJsxTaggedTemplate(token)) {
+				result.push({
+					start: token.template.getStart() + 1, // escape `
+					end: token.template.getEnd() - 1 // escape `
+				});
+				break;
+			}
+			else if (isStyledJsxTag(token)) {
+				result.push({
+					start: token.openingElement.getEnd() + 2, // escape `
+					end: token.closingElement.getStart() - 2 // escape `
+				});
+				break;
+			}
+			token = token.parent;
+		}
+	}
+	return result;
+}
+
+const expressionPattern = /(.*\${.*}.*)|(.*(&&|[||]).*)/g
+// I guess so long functions are bad. Don't know how to properly format in typescript.
+export function replaceAllWithSpacesExceptCss(textDocument: TextDocument, styledJsxTaggedTemplates: StyledJsxTaggedTemplate[], stylesheets: LanguageModelCache<Stylesheet>): { cssDocument: TextDocument, stylesheet: Stylesheet } {
+	const text = textDocument.getText();
+	let result = '';
+	// code that goes before CSS
+	result += text.slice(0, styledJsxTaggedTemplates[0].start).replace(/./g, ' ');
+	for (let i = 0; i < styledJsxTaggedTemplates.length; i++) {
+		// CSS itself with dirty hacks. Maybe there is better solution. 
+		// We need to find all expressions in css and replace each character of expression with space.
+		// This is neccessary to preserve character count
+		result += text.slice(styledJsxTaggedTemplates[i].start, styledJsxTaggedTemplates[i].end).replace(expressionPattern, (str, p1) => {
+			return p1.replace(/./g, ' ')
+		});
+		// if there is several CSS parts
+		if (i + 1 < styledJsxTaggedTemplates.length) {
+			// code that is in between that CSS parts
+			result  += text.slice(styledJsxTaggedTemplates[i].end, styledJsxTaggedTemplates[i+1].start).replace(/./g, ' ');
+		}
+	}
+	// code that goes after CSS
+	result += text.slice(styledJsxTaggedTemplates[styledJsxTaggedTemplates.length-1].end, text.length).replace(/./g, ' ');
+
+	const cssDocument = TextDocument.create(textDocument.uri.toString(), 'css', textDocument.version, result);
+	const stylesheet = stylesheets.get(cssDocument);
+
+	return {
+		cssDocument,
+		stylesheet
+	}
+}
+
+export function getStyledJsx(document: TextDocument, stylesheets: LanguageModelCache<Stylesheet>): StyledJsx | undefined {
+	const styledJsxOffsets = getApproximateStyledJsxOffsets(document);
+		if (styledJsxOffsets.length > 0) {
+			const styledJsxTaggedTemplates = findStyledJsxTaggedTemplate(document, styledJsxOffsets);
+			if (styledJsxTaggedTemplates.length > 0) {
+				return replaceAllWithSpacesExceptCss(document, styledJsxTaggedTemplates, stylesheets);
+			}
+		}
+	return undefined;
+}
+
+export function getStyledJsxUnderCursor(document: TextDocument, stylesheets: LanguageModelCache<Stylesheet>, cursorOffset: number): StyledJsx | undefined {
+	const styledJsxTaggedTemplates = findStyledJsxTaggedTemplate(document, [cursorOffset]);
+
+	if (styledJsxTaggedTemplates.length > 0 && styledJsxTaggedTemplates[0].start < cursorOffset && styledJsxTaggedTemplates[0].end > cursorOffset) {
+		return replaceAllWithSpacesExceptCss(document, styledJsxTaggedTemplates, stylesheets);
+	}
+	return undefined;
+}
